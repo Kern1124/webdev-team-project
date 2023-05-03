b8a86af8511fac316a9bae46614042a0b4ceb698
Jakub Tylich <xtylich@fi.muni.cz>
2023-05-03 12:10:51 +0200

    feat: vite dependency files

---
+++ frontend/.vite/deps/@chakra-ui_theme-utils.js
@@ -0,0 +1,20 @@
+import {
+  extendBaseTheme,
+  extendTheme,
+  mergeThemeOverride,
+  withDefaultColorScheme,
+  withDefaultProps,
+  withDefaultSize,
+  withDefaultVariant
+} from "./chunk-XTSWJVST.js";
+import "./chunk-AC2VUBZ6.js";
+export {
+  extendBaseTheme,
+  extendTheme,
+  mergeThemeOverride,
+  withDefaultColorScheme,
+  withDefaultProps,
+  withDefaultSize,
+  withDefaultVariant
+};
+//# sourceMappingURL=@chakra-ui_theme-utils.js.map
