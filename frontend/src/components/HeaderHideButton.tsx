d43afccf50444ad8f379504bfea382bb8518921d
Jakub Tylich <xtylich@fi.muni.cz>
2023-05-03 13:31:33 +0200

    feat: HeaderHideButton added

---
+++ frontend/src/components/HeaderHideButton.tsx
@@ -0,0 +1,33 @@
+import { Box } from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import { useCallback } from "react";
+import { boolean } from "yup";
+
+interface HeaderHideButtonProps {
+  isHidden: boolean;
+  setHidden: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const HeaderHideButton = ({
+  isHidden,
+  setHidden,
+}: HeaderHideButtonProps) => {
+  const toggleHidden = useCallback(() => {
+    setHidden((prev) => !prev);
+  }, [setHidden]);
+
+  return (
+    <Box
+      paddingLeft="0.5rem"
+      bgColor="main"
+      display={{ base: "block", md: "none" }}
+      onClick={toggleHidden}
+    >
+      {isHidden ? (
+        <HamburgerIcon margin="1rem" boxSize="2rem" color="secondaryLight" />
+      ) : (
+        <CloseIcon margin="1.25rem" boxSize="1.5rem" color="secondaryLight" />
+      )}
+    </Box>
+  );
+};
