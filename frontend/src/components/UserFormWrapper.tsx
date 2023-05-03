2867383d5d53b86063520b2bfc7edf9492873d46
Jakub Tylich <xtylich@fi.muni.cz>
2023-05-03 21:22:01 +0200

    feat: added onSubmit handler to UserFormWrapper

--- frontend/src/components/UserFormWrapper.tsx
+++ frontend/src/components/UserFormWrapper.tsx
@@ -9,22 +9,35 @@ import { UserFormButton } from "./UserFormButton";
 interface UserFormWrapperProps {
   heading: string;
   buttonLabel: string;
+  onSubmit: (
+    e?: React.BaseSyntheticEvent<object, any, any> | undefined
+  ) => Promise<void>;
   children: ReactElement<BoxProps> | ReactElement<BoxProps>[] | undefined;
 }
 
 export const UserFormWrapper = ({
   heading,
   children,
-  buttonLabel
+  buttonLabel,
+  onSubmit,
 }: UserFormWrapperProps) => {
   return (
     <ScaleFade in={true}>
       <Center marginTop="1.8rem">
-        <Card  marginBottom="5rem" bgColor="light" borderRadius="0.1rem" p={{base:"1.5rem" ,md:"3rem"}} w="30rem" color="main">
+        <Card
+          marginBottom="5rem"
+          bgColor="light"
+          borderRadius="0.1rem"
+          p={{ base: "1.5rem", md: "3rem" }}
+          w="30rem"
+          color="main"
+        >
           <CardHeader fontSize="3xl">{heading}</CardHeader>
           <Divider bgColor="main" />
           <CardBody marginTop="1rem">
-            <FormControl>{children}</FormControl>
+            <form onSubmit={onSubmit}>
+              <FormControl>{children}</FormControl>
+            </form>
           </CardBody>
           <CardFooter>
             <UserFormButton>{buttonLabel}</UserFormButton>
