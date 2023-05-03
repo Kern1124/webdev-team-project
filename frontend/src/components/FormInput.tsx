625d81aadaac44d3691ffa6de55a6830639ed2b1
Jakub Tylich <xtylich@fi.muni.cz>
2023-05-03 21:29:07 +0200

    fix: added FormInput register props

--- frontend/src/components/FormInput.tsx
+++ frontend/src/components/FormInput.tsx
@@ -1,33 +1,49 @@
-import { FormLabel } from "@chakra-ui/form-control";
-import { Input, InputGroup, InputLeftElement } from "@chakra-ui/input";
-import { IconType } from "react-icons";
-import { FieldValues, UseFormRegister } from "react-hook-form";
-import { Icon } from "@chakra-ui/react";
+import { FormLabel } from '@chakra-ui/form-control';
+import { Input, InputGroup, InputLeftElement } from '@chakra-ui/input';
+import { Icon } from '@chakra-ui/react';
+import { ChangeHandler } from 'react-hook-form';
+import { IconType } from 'react-icons';
 
 interface FormInputProps {
   children: string;
   placeholder: string;
   icon: IconType;
-  inputType?: string,
-  id: string,
-  register: UseFormRegister<FieldValues>
+  inputType?: string;
+  errorMessage?: string;
+  onChange: ChangeHandler;
+  onBlur: ChangeHandler;
+  ref: React.Ref<any>;
+  name: string;
 }
 
-export const FormInput = ({ children, placeholder, icon, inputType, id, register }: FormInputProps) => {
+export const FormInput = ({
+  children,
+  placeholder,
+  icon,
+  inputType,
+  errorMessage,
+  onChange,
+  onBlur,
+  ref,
+  name,
+}: FormInputProps) => {
   return (
     <>
       <FormLabel marginLeft="-0.9rem">{children}</FormLabel>
       <InputGroup>
-        <InputLeftElement pointerEvents="none" children={<Icon as={icon} />}  />
+        <InputLeftElement pointerEvents="none" children={<Icon as={icon} />} />
         <Input
-            id={id}
-            focusBorderColor="main"
-            marginBottom="1.5rem"
-            placeholder={placeholder}
-            variant="flushed"
-            type={inputType ?? "text"}
-            {...register(id)}
+          focusBorderColor="main"
+          marginBottom="1.5rem"
+          placeholder={placeholder}
+          variant="flushed"
+          type={inputType ?? "text"}
+          onChange={onChange}
+          onBlur={onBlur}
+          ref={ref}
+          name={name}
         />
+        {errorMessage ?? ""}
       </InputGroup>
     </>
   );
