93279a65c77ed61e2385b454d9bf100ddcef7d1b
Jakub Tylich <xtylich@fi.muni.cz>
2023-05-03 17:12:37 +0200

    fix: missing form component in UserFormWrapper

--- frontend/src/components/UserFormWrapper.tsx
+++ frontend/src/components/UserFormWrapper.tsx
@@ -1,21 +1,28 @@
 import { Card, CardBody, CardHeader } from "@chakra-ui/card";
-import { Center } from "@chakra-ui/layout";
+import { Center, Divider } from "@chakra-ui/layout";
 import { ScaleFade } from "@chakra-ui/transition";
 import { ReactElement } from "react";
 import { BoxProps } from "@chakra-ui/layout";
+import { FormControl } from "@chakra-ui/form-control";
 
 interface UserFormWrapperProps {
-    heading: string,
-    children: ReactElement<BoxProps> | ReactElement<BoxProps>[] | undefined
+  heading: string;
+  children: ReactElement<BoxProps> | ReactElement<BoxProps>[] | undefined;
 }
 
-export const UserFormWrapper = ({heading, children}: UserFormWrapperProps) => {
+export const UserFormWrapper = ({
+  heading,
+  children,
+}: UserFormWrapperProps) => {
   return (
     <ScaleFade in={true}>
       <Center marginTop="3.5rem">
-        <Card p="1.5rem" w="40rem" color="main">
+        <Card borderRadius="0.1rem" p="3rem" w="30rem" color="main">
           <CardHeader fontSize="3xl">{heading}</CardHeader>
-          <CardBody>{children}</CardBody>
+          <Divider bgColor="main" />
+          <CardBody marginTop="1rem">
+            <FormControl>{children}</FormControl>
+          </CardBody>
         </Card>
       </Center>
     </ScaleFade>
