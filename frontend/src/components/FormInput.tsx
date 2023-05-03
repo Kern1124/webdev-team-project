aefad008dbc226e2d9c5c3d3e1db2e03bff98082
Jakub Tylich <xtylich@fi.muni.cz>
2023-05-03 20:52:50 +0200

    feat: added hookform registration for FormInput

--- frontend/src/components/FormInput.tsx
+++ frontend/src/components/FormInput.tsx
@@ -1,7 +1,7 @@
 import { FormLabel } from "@chakra-ui/form-control";
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/input";
 import { IconType } from "react-icons";
-
+import { FieldValues, UseFormRegister } from "react-hook-form";
 import { Icon } from "@chakra-ui/react";
 
 interface FormInputProps {
@@ -9,20 +9,24 @@ interface FormInputProps {
   placeholder: string;
   icon: IconType;
   inputType?: string,
+  id: string,
+  register: UseFormRegister<FieldValues>
 }
 
-export const FormInput = ({ children, placeholder, icon, inputType }: FormInputProps) => {
+export const FormInput = ({ children, placeholder, icon, inputType, id, register }: FormInputProps) => {
   return (
     <>
       <FormLabel marginLeft="-0.9rem">{children}</FormLabel>
       <InputGroup>
         <InputLeftElement pointerEvents="none" children={<Icon as={icon} />}  />
         <Input
+            id={id}
             focusBorderColor="main"
             marginBottom="1.5rem"
             placeholder={placeholder}
             variant="flushed"
             type={inputType ?? "text"}
+            {...register(id)}
         />
       </InputGroup>
     </>
