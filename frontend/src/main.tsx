d4a17ef1c2a17dae6c7be1a6746aa0dea3e32fc2
Jakub Tylich <xtylich@fi.muni.cz>
2023-05-03 12:09:40 +0200

    feat: chakra provider added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./chakra-config/theme.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
