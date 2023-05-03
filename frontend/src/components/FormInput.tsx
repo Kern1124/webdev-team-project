8770c89fd45a8e2387f3d949d3928130be3cd209
Jakub Tylich <xtylich@fi.muni.cz>
2023-05-03 23:04:01 +0200

    feat: FormInput register and error messages

--- frontend/src/components/FormInput.tsx
+++ frontend/src/components/FormInput.tsx
@@ -1,8 +1,10 @@
-import { FormLabel } from '@chakra-ui/form-control';
-import { Input, InputGroup, InputLeftElement } from '@chakra-ui/input';
-import { Icon } from '@chakra-ui/react';
-import { ChangeHandler } from 'react-hook-form';
-import { IconType } from 'react-icons';
+import { FormLabel } from "@chakra-ui/form-control";
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/input";
+import { Icon, Text } from "@chakra-ui/react";
+import React from "react";
+import { LegacyRef } from "react";
+import { ChangeHandler } from "react-hook-form";
+import { IconType } from "react-icons";
 
 interface FormInputProps {
   children: string;
@@ -12,39 +14,48 @@ interface FormInputProps {
   errorMessage?: string;
   onChange: ChangeHandler;
   onBlur: ChangeHandler;
-  ref: React.Ref<any>;
   name: string;
 }
 
-export const FormInput = ({
-  children,
-  placeholder,
-  icon,
-  inputType,
-  errorMessage,
-  onChange,
-  onBlur,
-  ref,
-  name,
-}: FormInputProps) => {
-  return (
-    <>
-      <FormLabel marginLeft="-0.9rem">{children}</FormLabel>
-      <InputGroup>
-        <InputLeftElement pointerEvents="none" children={<Icon as={icon} />} />
-        <Input
-          focusBorderColor="main"
-          marginBottom="1.5rem"
-          placeholder={placeholder}
-          variant="flushed"
-          type={inputType ?? "text"}
-          onChange={onChange}
-          onBlur={onBlur}
-          ref={ref}
-          name={name}
-        />
-        {errorMessage ?? ""}
-      </InputGroup>
-    </>
-  );
-};
+export const FormInput = React.forwardRef(
+  (
+    {
+      children,
+      placeholder,
+      icon,
+      inputType,
+      errorMessage,
+      onChange,
+      onBlur,
+      name,
+    }: FormInputProps,
+    ref: LegacyRef<HTMLInputElement>
+  ) => {
+    return (
+      <>
+        <FormLabel marginLeft="-0.9rem">{children}</FormLabel>
+        <InputGroup flexDir="column">
+          <InputLeftElement
+            pointerEvents="none"
+            children={<Icon as={icon} />}
+          />
+          <Input
+            focusBorderColor="main"
+            marginBottom="0.3rem"
+            placeholder={placeholder}
+            variant="flushed"
+            type={inputType ?? "text"}
+            onChange={onChange}
+            onBlur={onBlur}
+            ref={ref}
+            name={name}
+            id={name}
+          />
+          <Text marginBottom="1rem" fontSize="sm" color="error">
+            {errorMessage ?? ""}
+          </Text>
+        </InputGroup>
+      </>
+    );
+  }
+);
