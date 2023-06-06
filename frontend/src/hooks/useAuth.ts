25da06575ff16a17a2e97f8d37080c1a36aa84f8
Jakub Tylich <xtylich@fi.muni.cz>
2023-06-06 17:24:36 +0200

    fix: useAuth api call

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,14 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
-import { auth } from "../api/requests";
+import { userAuth } from "../api/requests";
 
 export const useAuth = () => {
     const {data, isLoading, isError} = useQuery({
         queryKey: ["auth"],
         retry: false,
-        queryFn: () => auth(),
+        queryFn: () => userAuth(),
         staleTime: 1000 * 60 * 2,
         refetchOnWindowFocus: false
     })
-    
+
     return { auth: data, isLoading, isError}
 }
