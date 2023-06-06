5a2833b955c82910d99da5a2e8fa3c41aa5f83cd
Jakub Tylich <xtylich@fi.muni.cz>
2023-06-06 16:35:17 +0200

    feat: added api calls for session auth

--- frontend/src/api/requests.ts
+++ frontend/src/api/requests.ts
@@ -3,16 +3,29 @@ import { UserSignupFormType, UserFormType } from "../types/user"
 
 // TODO: change baseURL before release
 const serverApi = axios.create({
-    baseURL: "http://localhost:3000"
+    baseURL: "http://localhost:3000",
+    headers: {
+        "Access-Control-Allow-Credentials": true,
+    },
+    withCredentials: true
 })
 
-// TODO: replace placeholders
 export const userSignup = async (data: UserSignupFormType) => {
-    const response = await serverApi.post("PLACEHOLDER", data)
-    return response
+    const response = await serverApi.post("/register", data)
+    return response.data
 }
 
 export const userLogin = async (data: UserFormType) => {
-    const response = await serverApi.post("PLACEHOLDER", data)
-    return response
+    const response = await serverApi.post("/login", data)
+    return response.data
+}
+
+export const userLogout = async () => {
+    const response = await serverApi.post("/logout", {});
+    return response.data
+}
+
+export const useraAuth = async () => {
+    const response = await serverApi.get("/auth", {});
+    return response.data
 }
