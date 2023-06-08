b9ce9d9cdfbbba39fc5a5ae6be7d4d216ffd497c
Jan Ryzí <xryzi1@fi.muni.cz>
2023-06-08 05:32:01 +0200

    fix: seed

--- backend/src/seed/data.ts
+++ backend/src/seed/data.ts
@@ -2,9 +2,22 @@ import argon2 from 'argon2';
 import { RoleRecordType } from '../models/role';
 
 const data = async () => {
-  const hashedPassword1 = await argon2.hash('Slavoo');
-  const hashedPassword2 = await argon2.hash('Tylich');
+  /*
+  username: 'Slavo',
+            email: 'Slavo@example.com',
+            passwordHash: hashedPassword1,
+  */
+  const hashedPassword1 = await argon2.hash('Slavoo123');
+  /*
+  username: 'Tylich',
+            email: 'Tylich@example.com',
+            passwordHash: hashedPassword2,
+  */  
+  const hashedPassword2 = await argon2.hash('Tylich123');
   const hashedPassword3 = await argon2.hash('hashed_password3');
+  const hashedPassword4 = await argon2.hash('hashed_password4');
+  const hashedPassword5 = await argon2.hash('hashed_password5');
+  const hashedPassword6 = await argon2.hash('hashed_password6');
 
   return {
     publishers: [
@@ -52,7 +65,7 @@ const data = async () => {
         users: [
           {
             username: 'Slavo',
-            email: 'slavo@example.com',
+            email: 'Slavo@example.com',
             passwordHash: hashedPassword1,
             role: 'DIRECTOR' as RoleRecordType,
             articles: [
@@ -76,7 +89,7 @@ const data = async () => {
           },
           {
             username: 'Tylich',
-            email: 'tylich@example.com',
+            email: 'Tylich@example.com',
             passwordHash: hashedPassword2,
             role: 'MANAGER' as RoleRecordType,
             articles: [
@@ -99,8 +112,8 @@ const data = async () => {
             ],
           },
           {
-            username: 'User3',
-            email: 'user3@example.com',
+            username: 'jarmil',
+            email: 'Jarmil@example.com',
             passwordHash: hashedPassword3,
             articles: [
               {
@@ -113,6 +126,264 @@ const data = async () => {
               },
             ],
           },
+        ],
+      },
+      {
+        name: 'Publisher B',
+        newspapers: [
+          {
+            name: 'Newspaper A',
+            newspaperCopies: [
+              {
+                articles: [
+                  {
+                    contents: 'Article 1 for Newspaper A Issue 1',
+                    categories: [{ name: 'Category A' }],
+                    comments: [
+                      { content: 'Comment 1 for Article 1', author: { username: 'User1' } },
+                      { content: 'Comment 2 for Article 1', author: { username: 'User2' } },
+                    ],
+                  },
+                  {
+                    contents: 'Article 2 for Newspaper A Issue 1',
+                    categories: [{ name: 'Category B' }],
+                    comments: [
+                      { content: 'Comment 1 for Article 2', author: { username: 'User1' } },
+                      { content: 'Comment 2 for Article 2', author: { username: 'User3' } },
+                    ],
+                  },
+                ],
+              },
+              {
+                articles: [
+                  {
+                    contents: 'Article 1 for Newspaper A Issue 2',
+                    categories: [{ name: 'Category A' }],
+                    comments: [
+                      { content: 'Comment 1 for Article 1', author: { username: 'User2' } },
+                      { content: 'Comment 2 for Article 1', author: { username: 'User3' } },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+        users: [
+          {
+            username: 'Vojtech',
+            email: 'Vojtech@example.com',
+            passwordHash: hashedPassword4,
+            role: 'DIRECTOR' as RoleRecordType,
+            articles: [
+              {
+                contents: 'Article 1 by User1',
+                categories: [{ name: 'Category A' }],
+                comments: [
+                  { content: 'Comment 1 for Article 1 by User1' },
+                  { content: 'Comment 2 for Article 1 by User1' },
+                ],
+              },
+              {
+                contents: 'Article 2 by User1',
+                categories: [{ name: 'Category B' }],
+                comments: [
+                  { content: 'Comment 1 for Article 2 by User1' },
+                  { content: 'Comment 2 for Article 2 by User1' },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: 'Publisher C',
+        newspapers: [
+          {
+            name: 'Newspaper A',
+            newspaperCopies: [
+              {
+                articles: [
+                  {
+                    contents: 'Article 1 for Newspaper A Issue 1',
+                    categories: [{ name: 'Category A' }],
+                    comments: [
+                      { content: 'Comment 1 for Article 1', author: { username: 'User1' } },
+                      { content: 'Comment 2 for Article 1', author: { username: 'User2' } },
+                    ],
+                  },
+                  {
+                    contents: 'Article 2 for Newspaper A Issue 1',
+                    categories: [{ name: 'Category B' }],
+                    comments: [
+                      { content: 'Comment 1 for Article 2', author: { username: 'User1' } },
+                      { content: 'Comment 2 for Article 2', author: { username: 'User3' } },
+                    ],
+                  },
+                ],
+              },
+              {
+                articles: [
+                  {
+                    contents: 'Article 1 for Newspaper A Issue 2',
+                    categories: [{ name: 'Category A' }],
+                    comments: [
+                      { content: 'Comment 1 for Article 1', author: { username: 'User2' } },
+                      { content: 'Comment 2 for Article 1', author: { username: 'User3' } },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+        users: [
+          {
+            username: 'Honza',
+            email: 'Honza@example.com',
+            passwordHash: hashedPassword5,
+            role: 'DIRECTOR' as RoleRecordType,
+            articles: [
+              {
+                contents: 'Article 1 by User1',
+                categories: [{ name: 'Category A' }],
+                comments: [
+                  { content: 'Comment 1 for Article 1 by User1' },
+                  { content: 'Comment 2 for Article 1 by User1' },
+                ],
+              },
+              {
+                contents: 'Article 2 by User1',
+                categories: [{ name: 'Category B' }],
+                comments: [
+                  { content: 'Comment 1 for Article 2 by User1' },
+                  { content: 'Comment 2 for Article 2 by User1' },
+                ],
+              },
+            ],
+          },
+          {
+            username: 'Hanys',
+            email: 'Hanys@example.com',
+            passwordHash: hashedPassword2,
+            role: 'MANAGER' as RoleRecordType,
+            articles: [
+              {
+                contents: 'Article 1 by User2',
+                categories: [{ name: 'Category A' }],
+                comments: [
+                  { content: 'Comment 1 for Article 1 by User2' },
+                  { content: 'Comment 2 for Article 1 by User2' },
+                ],
+              },
+              {
+                contents: 'Article 2 by User2',
+                categories: [{ name: 'Category B' }],
+                comments: [
+                  { content: 'Comment 1 for Article 2 by User2' },
+                  { content: 'Comment 2 for Article 2 by User2' },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: 'Publisher D',
+        newspapers: [
+          {
+            name: 'Newspaper A',
+            newspaperCopies: [
+              {
+                articles: [
+                  {
+                    contents: 'Article 1 for Newspaper A Issue 1',
+                    categories: [{ name: 'Category A' }],
+                    comments: [
+                      { content: 'Comment 1 for Article 1', author: { username: 'User1' } },
+                      { content: 'Comment 2 for Article 1', author: { username: 'User2' } },
+                    ],
+                  },
+                  {
+                    contents: 'Article 2 for Newspaper A Issue 1',
+                    categories: [{ name: 'Category B' }],
+                    comments: [
+                      { content: 'Comment 1 for Article 2', author: { username: 'User1' } },
+                      { content: 'Comment 2 for Article 2', author: { username: 'User3' } },
+                    ],
+                  },
+                ],
+              },
+              {
+                articles: [
+                  {
+                    contents: 'Article 1 for Newspaper A Issue 2',
+                    categories: [{ name: 'Category A' }],
+                    comments: [
+                      { content: 'Comment 1 for Article 1', author: { username: 'User2' } },
+                      { content: 'Comment 2 for Article 1', author: { username: 'User3' } },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+        users: [
+        ],
+      },
+      {
+        name: 'Publisher E',
+        newspapers: [
+          {
+            name: 'Newspaper A',
+            newspaperCopies: [
+              {
+                articles: [
+                  {
+                    contents: 'Article 1 for Newspaper A Issue 1',
+                    categories: [{ name: 'Category A' }],
+                    comments: [
+                      { content: 'Comment 1 for Article 1', author: { username: 'User1' } },
+                      { content: 'Comment 2 for Article 1', author: { username: 'User2' } },
+                    ],
+                  },
+                  {
+                    contents: 'Article 2 for Newspaper A Issue 1',
+                    categories: [{ name: 'Category B' }],
+                    comments: [
+                      { content: 'Comment 1 for Article 2', author: { username: 'User1' } },
+                      { content: 'Comment 2 for Article 2', author: { username: 'User3' } },
+                    ],
+                  },
+                ],
+              },
+              {
+                articles: [
+                  {
+                    contents: 'Article 1 for Newspaper A Issue 2',
+                    categories: [{ name: 'Category A' }],
+                    comments: [
+                      { content: 'Comment 1 for Article 1', author: { username: 'User2' } },
+                      { content: 'Comment 2 for Article 1', author: { username: 'User3' } },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+        users: [
+         
+        ],
+      },
+
+      {
+        name: 'Publisher F',
+        newspapers: [
+        ],
+        users: [
+          
         ],
       },
     ],
