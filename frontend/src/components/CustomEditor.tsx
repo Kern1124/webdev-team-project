de60baa0ec245507217c933cb706c60f6ab1d219
Jakub Tylich <xtylich@fi.muni.cz>
2023-06-09 11:47:57 +0200

    feat: created a custom article editor

---
+++ frontend/src/components/CustomEditor.tsx
@@ -0,0 +1,95 @@
+import "react-quill/dist/quill.snow.css";
+
+import { Button } from "@chakra-ui/button";
+import { FormControl, FormLabel } from "@chakra-ui/form-control";
+import { useDisclosure } from "@chakra-ui/hooks";
+import { Input } from "@chakra-ui/input";
+import { Box } from "@chakra-ui/layout";
+import {
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/modal";
+import { ChangeEvent, useCallback, useRef, useState } from "react";
+import ReactQuill from "react-quill";
+
+export const CustomEditor = () => {
+  const [value, setValue] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const reactQuillRef: any = useRef(null);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const imageHandler = useCallback(() => {
+    if (imageUrl) {
+      onClose();
+      const quill = reactQuillRef?.current?.getEditor();
+      quill.focus();
+      const range = quill.getSelection();
+      quill.insertEmbed(range.index, "image", imageUrl);
+      quill.setSelection(range.index + 1);
+      setImageUrl("");
+    }
+  }, [imageUrl, onClose]);
+
+  const inputChangeHandler = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => setImageUrl(e.target.value),
+    []
+  );
+
+  return (
+    <>
+      <Box h="20vh" w={{ base: "100%", md: "70%" }}>
+        <ReactQuill
+          value={value}
+          onChange={setValue}
+          modules={{
+            toolbar: {
+              container: [
+                [{ header: [1, 2, false] }],
+                ["bold", "italic", "underline", "strike", "blockquote"],
+                [
+                  { list: "ordered" },
+                  { list: "bullet" },
+                  { indent: "-1" },
+                  { indent: "+1" },
+                ],
+                ["link", "image"],
+                ["clean"],
+              ],
+              handlers: {
+                image: onOpen,
+              },
+            },
+          }}
+          placeholder="Input article content..."
+          ref={reactQuillRef}
+        />
+      </Box>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Input image URL</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              <FormLabel>URL</FormLabel>
+              <Input placeholder="url" onChange={inputChangeHandler} />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button bgColor="main" color="light" mr={3} onClick={imageHandler}>
+              Add
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
