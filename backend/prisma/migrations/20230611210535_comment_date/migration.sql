d163cdebb4c0e4cc81bb9dfd943936c251f57578
Jan Ryzí <xryzi1@fi.muni.cz>
2023-06-11 23:15:04 +0200

    fix: comment date

---
+++ backend/prisma/migrations/20230611210535_comment_date/migration.sql
@@ -0,0 +1,16 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Comment" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "content" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "articleId" TEXT NOT NULL,
+    "authorId" TEXT NOT NULL,
+    CONSTRAINT "Comment_articleId_fkey" FOREIGN KEY ("articleId") REFERENCES "Article" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Comment_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Comment" ("articleId", "authorId", "content", "id") SELECT "articleId", "authorId", "content", "id" FROM "Comment";
+DROP TABLE "Comment";
+ALTER TABLE "new_Comment" RENAME TO "Comment";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
--- backend/src/api/comment/commentApi.ts
+++ backend/src/api/comment/commentApi.ts
@@ -10,57 +10,70 @@ import { commentCreateSchema } from "../../models/comment";
 import { CreateCommentData } from "../../types/comment.types";
 
 const create = async (req: Request, res: Response) => {
-    try {
-        const authorId = req.session.user?.id;
-        if (!authorId) {
-            res.status(401).json({ message: "User is not authenticated" });
-            return;
-        }
-        const validatedData = await commentCreateSchema.validate(req.body);
-        const commentData = validatedData as CreateCommentData;
-
-        const comment = await commentService.default.create({ ...commentData, authorId });
-        if (comment.isErr) {
-            res.status(400).json({ message: comment.error })
-            return;
-        }
-        res.status(200).json({ item: comment.value, message: `Comment was created by ${req.session.user?.username} successfully.` });
-        return;
-    } catch (error) {
-        if (error instanceof ValidationError) {
-            res.status(400).json({ message: error.message });
-            return;
-        }
-        res.status(500).json({ message: (error as Error).message });
+  try {
+    const authorId = req.session.user?.id;
+    if (!authorId) {
+      res.status(401).json({ message: "User is not authenticated" });
+      return;
     }
+    const validatedData = await commentCreateSchema.validate(req.body);
+    const commentData = validatedData as CreateCommentData;
+
+    const comment = await commentService.default.create({ ...commentData, authorId });
+    if (comment.isErr) {
+      res.status(400).json({ message: comment.error })
+      return;
+    }
+    res.status(200).json({ item: comment.value, message: `Comment was created by ${req.session.user?.username} successfully.` });
+    return;
+  } catch (error) {
+    if (error instanceof ValidationError) {
+      res.status(400).json({ message: error.message });
+      return;
+    }
+    res.status(500).json({ message: (error as Error).message });
+  }
 };
 
 const getArticleComments = async (req: Request, res: Response) => {
-    try {
-        const articleId = req.params.articleId
-        const article = await db.article.findFirst({
-            where: {
-                id: articleId,
-            }
-        })
-        if (!article) {
-            return res.status(400).json({ message: "Article with this id doesn't exist." })
-        }
-        const comments = await db.comment.findMany({
-            where: {
-                articleId,
-            },
-        })
-        res.status(200).json({ item: comments, message: "Fetched " + comments.length + " comments." })
-    } catch (e) {
-        res.status(500).json({ message: "Something went wrong.", error: e })
+  try {
+    const articleId = req.params.articleId
+    const article = await db.article.findFirst({
+      where: {
+        id: articleId,
+      },
+    })
+    if (!article) {
+      return res.status(400).json({ message: "Article with this id doesn't exist." })
     }
+    // add order by created At
+    const comments = await db.comment.findMany({
+      where: {
+        articleId,
+      },
+      select: {
+        author: {
+          select: {
+            username: true,
+          },
+        },
+        createdAt: true,
+        content: true,
+      },
+      orderBy: {
+        createdAt: 'asc'
+      }
+    })
+    res.status(200).json({ item: comments, message: "Fetched " + comments.length + " comments." })
+  } catch (e) {
+    res.status(500).json({ message: "Something went wrong.", error: e })
+  }
 }
 
 
 
 
 export const commentApi = {
-    create,
-    getArticleComments,
+  create,
+  getArticleComments,
 };
