33ee48ace66806630420f5c0c871b57e4eb08e2e
Jakub Tylich <xtylich@fi.muni.cz>
2023-06-11 13:20:47 +0200

    feat: added article comments component

---
+++ frontend/src/components/ArticleComments.tsx
@@ -0,0 +1,27 @@
+import { CommentList } from "./CommentList";
+import { ArticleCommentsWrapper } from "./ArticleCommentsWrapper";
+import { CommentSubmit } from "./CommentSubmit";
+import { SubpageHeading } from "./SubpageHeading";
+
+const SAMPLE_COMMENTS = [
+  {
+    id: "1",
+    content:
+      "Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1",
+    author: "TestUser1",
+    createdAt: "12.2.2022",
+  },
+  { id: "2", content: "Test2", author: "TestUser2", createdAt: "12.3.2022" },
+  { id: "3", content: "Test3", author: "TestUser3", createdAt: "12.4.2022" },
+  { id: "4", content: "Test4", author: "TestUser4", createdAt: "12.5.2022" },
+];
+
+export const ArticleComments = () => {
+  return (
+    <ArticleCommentsWrapper>
+      <SubpageHeading heading="Comments" />
+      <CommentList comments={SAMPLE_COMMENTS} />
+      <CommentSubmit buttonLabel="Submit comment" />
+    </ArticleCommentsWrapper>
+  );
+};
