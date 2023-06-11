340ee73bf735b7e34cfc674bb89ab7cd5c6dae56
xjanos <xjanos@fi.muni.cz>
2023-06-11 22:55:54 +0200

    fix(): margin px to rem

--- frontend/src/components/ArticleContent.tsx
+++ frontend/src/components/ArticleContent.tsx
@@ -7,7 +7,7 @@ interface ArticleContentProp {
 
 export const ArticleContent = ({contents}: ArticleContentProp) => {
     return (
-        <Flex margin={2} flex={1} width={'100%'}>
+        <Flex margin={'1rem'} flex={1} width={'100%'}>
             <Box flex={1} flexGrow={3.5} marginLeft={'20%'}>
                 <div dangerouslySetInnerHTML={ {__html: contents ?? ""}}/>
             </Box>
