de8252924ee6031a4047f495a5f0a4b8cb582ad5
Jan Ryzí <xryzi1@fi.muni.cz>
2023-06-11 17:24:26 +0200

    fix: auth roles

--- backend/src/api/user/userApi.ts
+++ backend/src/api/user/userApi.ts
@@ -7,6 +7,7 @@ import { db } from "../../utils/db.server";
 import { ValidationError } from "yup";
 import { Role, User } from "@prisma/client";
 import { roles } from "../../seed/data";
+import { RoleRecordType, RoleRecordTypeEnumeration } from "../../models/role";
 
 // We are using express-validator to validate whether requests should be successful.
 // You can read more about it here: https://express-validator.github.io/docs/guides/getting-started
@@ -16,10 +17,17 @@ const auth = async (req: Request, res: Response) => {
     try {
         const user = req.session.user;
         if (!user) {
-            return res.status(200).json({ message: 'Unauthorized' });
+            return res.status(200).json({ items: [], message: `User is unauthorized` });
+        }
+        let roles: RoleRecordType[] = [RoleRecordTypeEnumeration[2]];
+        if (user.userRoles.some(role => role.name === RoleRecordTypeEnumeration[1])) {
+            roles.push(RoleRecordTypeEnumeration[1]);
+        }
+        if (user.userRoles.some(role => role.name === RoleRecordTypeEnumeration[0])) {
+            roles.push(RoleRecordTypeEnumeration[0]);
         }
         // user.userRoles.map((roles) => roles.name).nub //like Haskell nub
-        res.status(200).json({items: {...user}, message: `User ${user.username} is authorized` });
+        res.status(200).json({ items: roles, message: `User ${user.username} is authorized` });
     }
     catch (error) {
         res.status(500).json({ message: (error as Error).message });
