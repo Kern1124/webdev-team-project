5142baff11ceb66f46f62d97409ad2c55223ae5a
Jakub Tylich <xtylich@fi.muni.cz>
2023-06-12 16:00:14 +0200

    fix(ArticleItem): article item button alingment

--- frontend/src/components/ApprovalAction.tsx
+++ frontend/src/components/ApprovalAction.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import { useApproval } from "../hooks/useApproval";
 import { useCallback } from "react";
 import { CustomButton } from "./CustomButton";
@@ -8,9 +8,14 @@ type ApprovalActionProps = {
   discardName: string;
   approveUrl: string;
   discardUrl: string;
-}
+};
 
-export const ApprovalAction = ({ approveName, discardName, approveUrl, discardUrl }: ApprovalActionProps) => {
+export const ApprovalAction = ({
+  approveName,
+  discardName,
+  approveUrl,
+  discardUrl,
+}: ApprovalActionProps) => {
   const { approval } = useApproval();
 
   const handleApprove = useCallback(() => {
@@ -22,15 +27,9 @@ export const ApprovalAction = ({ approveName, discardName, approveUrl, discardUr
   }, [approval, discardUrl]);
 
   return (
-    <Box display="flex" alignItems="center">
-      <Box mr="1rem">
-        <CustomButton onClickHandler={handleApprove}>
-          {approveName}
-        </CustomButton>
-      </Box>
-      <CustomButton onClickHandler={handleDiscard}>
-        {discardName}
-      </CustomButton>
-    </Box>
+    <Flex flexDir={{ base: "column", md: "row" }} gap="0.5rem">
+      <CustomButton onClickHandler={handleApprove}>{approveName}</CustomButton>
+      <CustomButton onClickHandler={handleDiscard}>{discardName}</CustomButton>
+    </Flex>
   );
 };
--- frontend/src/components/ArticleItem.tsx
+++ frontend/src/components/ArticleItem.tsx
@@ -1,19 +1,24 @@
-import { Box, Flex} from '@chakra-ui/react';
+import { Box, Flex } from "@chakra-ui/react";
 import { ArticleHeading } from "./ArticleHeading";
 import { ArticleCategories } from "./ArticleCategories";
 import { ApprovalAction } from "./ApprovalAction";
 import { Article } from "../types/article";
-import { Authorized } from './Authorized';
+import { Authorized } from "./Authorized";
 
 type ArticleItemProps = {
   article: Article;
-}
+};
 
 export const ArticleItem = ({ article }: ArticleItemProps) => {
   return (
     <Box>
       <Flex align="center" justifyContent="space-between" flexDir="row">
-        <ArticleHeading heading={article.heading} id={article.id} />
+        <Flex flexDir="column">
+          <ArticleHeading heading={article.heading} id={article.id} />
+          <ArticleCategories
+            categories={article.categories.map((category) => category.name)}
+          />
+        </Flex>
         <Authorized role={"MANAGER"} condition={!article.approved}>
           <ApprovalAction
             approveName="Approve"
@@ -23,7 +28,6 @@ export const ArticleItem = ({ article }: ArticleItemProps) => {
           />
         </Authorized>
       </Flex>
-      <ArticleCategories categories={article.categories.map(category => category.name)} />
     </Box>
   );
-}
+};
