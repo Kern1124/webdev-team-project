e8c0a16a54aad89d53f4d913113f2b44e7212686
xjanos <xjanos@fi.muni.cz>
2023-06-12 11:06:58 +0200

    feat(ArticleItem): update for the branch

--- frontend/src/components/ArticleItem.tsx
+++ frontend/src/components/ArticleItem.tsx
@@ -1,28 +1,29 @@
-import { Box } from '@chakra-ui/react';
-import { useAuth } from "../hooks/useAuth";
+import { Box, Flex, Spacer } from '@chakra-ui/react';
 import { ArticleHeading } from "./ArticleHeading";
 import { ArticleCategories } from "./ArticleCategories";
 import { ApprovalAction } from "./ApprovalAction";
 import { Article } from "../types/article";
+import { Authorized } from './Authorized';
 
 type ArticleItemProps = {
   article: Article;
 }
 
 export const ArticleItem = ({ article }: ArticleItemProps) => {
-  const { auth } = useAuth();
-  const userRole = auth.item.userRole;
-
   return (
-    <Box mb={5}>
-      <ArticleHeading heading={article.heading} />
+    <Box>
+      <Flex align="center" justifyContent="space-between" flexDir="row">
+        <Box><ArticleHeading heading={article.heading} /></Box>
+        <Box><Authorized role={"MANAGER"} condition={!article.approved}>
+          <ApprovalAction
+            approveName="Approve"
+            discardName="Discard"
+            approveUrl={`/articles/${article.id}/approve`}
+            discardUrl={`/articles/${article.id}/discard`}
+          />
+        </Authorized></Box>
+      </Flex>
       <ArticleCategories categories={article.categories.map(category => category.name)} />
-      {userRole === 'MANAGER' && !article.approved &&
-        <ApprovalAction
-          approve={{name: "Approve", link: "/approve-link"}}
-          discard={{name: "Discard", link: "/discard-link"}}
-        />
-      }
     </Box>
   );
-};
+}
