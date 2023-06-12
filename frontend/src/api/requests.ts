66b0ae8928df69522534b5c795b0983e47bc9146
Jakub Tylich <xtylich@fi.muni.cz>
2023-06-12 13:15:10 +0200

    feat: added getComments query

--- frontend/src/api/requests.ts
+++ frontend/src/api/requests.ts
@@ -1,87 +1,91 @@
-import axios from "axios"
-import { UserSignupFormType, UserFormType } from "../types/user"
-import { CommentSubmitType } from "../types/comment"
-import { ArticleFormType } from "../types/article"
+import axios from "axios";
+import { UserSignupFormType, UserFormType } from "../types/user";
+import { CommentSubmitType } from "../types/comment";
+import { ArticleFormType } from "../types/article";
 
 // TODO: change baseURL before release
 const serverApi = axios.create({
-    baseURL: 'http://localhost:8000',
-    headers: {
-        "Access-Control-Allow-Credentials": true,
-    },
-    withCredentials: true
-})
-
+  baseURL: "http://localhost:8000",
+  headers: {
+    "Access-Control-Allow-Credentials": true,
+  },
+  withCredentials: true,
+});
 
 export const userSignup = async (data: UserSignupFormType) => {
-    const response = await serverApi.post("/register", data)
-    return response.data
-}
+  const response = await serverApi.post("/register", data);
+  return response.data;
+};
 
 export const userLogin = async (data: UserFormType) => {
-    const response = await serverApi.post("/login", data)
-    return response.data
-}
+  const response = await serverApi.post("/login", data);
+  return response.data;
+};
 
 export const userLogout = async () => {
-    const response = await serverApi.post("/logout", {});
-    return response.data
-}
+  const response = await serverApi.post("/logout", {});
+  return response.data;
+};
 
 export const userAuth = async () => {
-    const response = await serverApi.get("/auth", {});
-    return response.data
-}
+  const response = await serverApi.get("/auth", {});
+  return response.data;
+};
 
 export const getAllPublishers = async () => {
-    const response = await serverApi.get("/publisher", {})
-    return response.data
-}
+  const response = await serverApi.get("/publisher", {});
+  return response.data;
+};
 
 export const getNewspapers = async (publisherId: string, title: string) => {
-    let url = "/newspaper";
-    if (publisherId && title) {
-        url += "/" + publisherId + "/" + title;
-    } else if (publisherId) {
-        url += "/publisher=/" + publisherId;
-    } else if (title) {
-        url += "/name=/" + title;
-    } 
-    
-    const response = await serverApi.get(url, {})
-    return response.data
-}
+  let url = "/newspaper";
+  if (publisherId && title) {
+    url += "/" + publisherId + "/" + title;
+  } else if (publisherId) {
+    url += "/publisher=/" + publisherId;
+  } else if (title) {
+    url += "/name=/" + title;
+  }
+
+  const response = await serverApi.get(url, {});
+  return response.data;
+};
 
 export const getNewspaper = async (newspaperId: string) => {
-    const response = await serverApi.get(`/newspaper/${newspaperId}`);
-    return response.data
-}
+  const response = await serverApi.get(`/newspaper/${newspaperId}`);
+  return response.data;
+};
 
 export const getCategories = async () => {
-    const response = await serverApi.get("/category", {})
-    return response.data
-}
+  const response = await serverApi.get("/category", {});
+  return response.data;
+};
 
 export const postComment = async (data: CommentSubmitType) => {
-    const response = await serverApi.post("/article/comment", data)
-    return response.data
-}
+  const response = await serverApi.post("/article/comment", data);
+  return response.data;
+};
 
-export const postApproval = async (url: string ) => {
-    const response = await serverApi.post(url);
-    return response.data;
-}
+export const postApproval = async (url: string) => {
+  const response = await serverApi.post(url);
+  return response.data;
+};
 
 export const submitArticle = async (data: ArticleFormType) => {
-    const response = await serverApi.post("/article/create", data)
-    return response.data
-}
+  const response = await serverApi.post("/article/create", data);
+  return response.data;
+};
 export const getArticleWithId = async (articleId: string) => {
-    const response = await serverApi.get(`/article/${articleId}`)
-    return response.data
-}
+  const response = await serverApi.get(`/article/${articleId}`);
+  return response.data;
+};
 
 export const getRelatedArticles = async (articleId: string) => {
-    const response = await serverApi.get(`/article/related/${articleId}`)
-    return response.data
-}
+  const response = await serverApi.get(`/article/related/${articleId}`);
+  return response.data;
+};
+
+export const getComments = async (articleId: string) => {
+  const response = await serverApi.get(`/article/${articleId}/comment`, {});
+  return response.data;
+};
--- frontend/src/components/ArticleComments.tsx
+++ frontend/src/components/ArticleComments.tsx
@@ -6,19 +6,6 @@ import { Authorized } from "./Authorized";
 import { getComments } from "../api/requests";
 import { useQuery } from "@tanstack/react-query";
 
-const SAMPLE_COMMENTS = [
-  {
-    id: "1",
-    content:
-      "Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1Test1",
-    author: "TestUser1",
-    createdAt: "12.2.2022",
-  },
-  { id: "2", content: "Test2", author: "TestUser2", createdAt: "12.3.2022" },
-  { id: "3", content: "Test3", author: "TestUser3", createdAt: "12.4.2022" },
-  { id: "4", content: "Test4", author: "TestUser4", createdAt: "12.5.2022" },
-];
-
 interface ArticleCommentsProps {
   articleId: string;
 }
